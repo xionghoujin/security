15fc8d14f8ff9aa508af4457a9b21632fc9b5653
xionghoujin <892989054@qq.com>
2018-05-23 21:10:09 +0800

    securityDemo

---
+++ security-app/src/main/java/com/xhj/securityapp/SecurityAppApplication.java
@@ -0,0 +1,12 @@
+package com.xhj.securityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAppApplication.class, args);
+	}
+}
---
+++ security-core/src/main/java/com/xhj/securitycore/SecurityCoreApplication.java
@@ -0,0 +1,12 @@
+package com.xhj.securitycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCoreApplication.class, args);
+	}
+}
